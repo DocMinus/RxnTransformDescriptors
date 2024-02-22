8ad2a8f300abf42d25c2fac9359dce68bd597849
DocMinus <alex@pharmakarma.org>
2024-02-22 23:15:49 +0100

    setup created

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="td_tools",
+    version="2.1.3",
+    packages=find_packages(),
+    package_data={
+        "td_tools": ["*.txt"],
+    },
+    description="Reaction Transform Descriptor Tools",
+    author="DocMinus",
+    author_email="alexander.minidis@gmail.com",
+    url="https://github.com/DocMinus/RxnTransformDescriptors",
+)
