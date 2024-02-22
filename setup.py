7d27477354e2b0c8057501d229c999ced626a927
DocMinus <alex@pharmakarma.org>
2024-02-22 23:21:58 +0100

    setup added and updated

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="td_tools",
     version="2.1.3",
+    pythonrequires=">=3.9",
     packages=find_packages(),
     package_data={
         "td_tools": ["*.txt"],
